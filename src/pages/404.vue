08e3f696d1520b73b43c8742e1ef1209a9dd40b4
Bong Nguyen <van.bong@grabtaxi.com>
2020-11-02 17:17:16 +0800

    feat: add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="w-full flex justify-center">
+    <article class="w-full max-w-2xl">
+      <div class="pt-8 md:pt-16">
+        <h3 class="text-center font-bold text-2xl">
+          Oh no! There is nothing here.
+        </h3>
+      </div>
+      <div class="mb-10 flex items-center justify-center">
+        <g-link to="/" class="no-underline text-primary hover:underline"
+          >Take me home.</g-link
+        >
+      </div>
+    </article>
+  </main>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "404 - Nothing here",
+  },
+};
+</script>
